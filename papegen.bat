fec86459d8ff7c46c7417854a05896fcd30f364a
Michael del Rosario <15952601+mikejdelro@users.noreply.github.com>
2019-07-22 09:58:21 -0700

    cleaned up waifu2x.exe command

--- papegen.bat
+++ papegen.bat
@@ -3,7 +3,7 @@ SETLOCAL ENABLEEXTENSIONS
 ::This file should 
 
 ECHO Checking if you have waifu2x-caffe in %cd%\waifu2x-caffe
-if exist "%cd%\waifu2x-caffe\waifu2x-caffe.exe" (
+if exist "%cd%\waifu2x-caffe\waifu2x-caffe-cui.exe" (
     goto :haswf
 ECHO You have waifu2x-caffe.
 ) else goto :dlwf
@@ -13,7 +13,6 @@ ECHO You have waifu2x-caffe.
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lltcggie/waifu2x-caffe/releases/download/1.2.0.2/waifu2x-caffe.zip', 'waifu2x-caffe.zip')"
     ::powershell Invoke-WebRequest -Uri "https://github.com/lltcggie/waifu2x-caffe/releases/download/1.2.0.2/waifu2x-caffe.zip" -OutFile "%cd%\waifu2x-caffe.zip"
     powershell Expand-Archive waifu2x-caffe.zip -DestinationPath %cd%
-    cls
     ECHO You now have waifu2x-caffe
     del waifu2x-caffe.zip
 
@@ -29,7 +28,6 @@ ECHO You have waifu2x-caffe.
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-7.0.8-56-portable-Q16-x64.zip', 'imagemagick.zip')"
     ::powershell Invoke-WebRequest -Uri "ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-7.0.8-56-portable-Q16-x64.zip" -OutFile "%cd%\imagemagick.zip"
     powershell Expand-Archive imagemagick.zip -DestinationPath %cd%\imagemagick
-    cls
     ECHO You now have imagemagick
     del imagemagick.zip
 
@@ -85,14 +83,15 @@ ECHO 3 - Create HD TALL Wallpapers (1440x2560)
 ECHO 4 - Create HD WIDE Wallpapers (2560x1440)
 ECHO 5 - Create ALL WIDE Wallpapers (1920x1080 AND 2560x1440)
 ECHO 6 - Create ALL TALL Wallpapers (1080x1920 AND 1440x2560)
-ECHO 0 - Exit
-choice /n /c:0123456 /M "Choose an option (0-6) "
+ECHO 9 - Exit
+choice /n /c:1234569 /M "Choose an option (0-96) "
 
 if "%ERRORLEVEL%" == "0" (
     goto prog_exit
 )
 
 if "%ERRORLEVEL%" == "1" (
+    set img_tall=1
     set img_width=1080
     set img_height=1920
 )
@@ -101,6 +100,7 @@ if "%ERRORLEVEL%" == "2" (
     set img_height=1080
 )
 if "%ERRORLEVEL%" == "3" (
+    set img_tall=1
     set img_width=2560
     set img_height=1440
 )
@@ -116,6 +116,7 @@ if "%ERRORLEVEL%" == "5" (
     set img_height_extra=1440
 )
 if "%ERRORLEVEL%" == "6" (
+    set img_tall=1
     set has_extra=1
     set img_width=1920
     set img_height=1080
@@ -124,10 +125,10 @@ if "%ERRORLEVEL%" == "6" (
 )
 
 if %img_tall% == "1" (
-    %cd%\waifu2x-caffe\waifu2x-caffe.exe -t %tta_mode% -p %cudnn_mode% -i %cd%\input -o %cd%\input\temp\*.png -h %img_height%
+    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -h %img_height% -n 2 -m auto_scale -e png -o %cd%\input\temp -i %cd%\input
     %cd%\imagemagick\mogrify.exe %cd%\input\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
 ) else (
-    %cd%\waifu2x-caffe\waifu2x-caffe.exe -t %tta_mode% -p %cudnn_mode% -i %cd%\input -o %cd%\input\temp\*.png -w %img_width%
+    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -w %img_width% -n 2 -m auto_scale -e png -o %cd%\input\temp -i %cd%\input
     %cd%\imagemagick\mogrify.exe %cd%\input\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
 )
 
