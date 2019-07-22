a243d394e076a8c01237ef8047daf85bb6f78cbc
Michael del Rosario <mikejdelro@gmail.com>
2019-07-22 13:49:42 -0700

    Added TODO

--- papegen.bat
+++ papegen.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
+::No clue what this does yet but it looks cool
 SETLOCAL ENABLEEXTENSIONS
-::This file should 
 
 ECHO Checking if you have waifu2x-caffe in %cd%\waifu2x-caffe
 if exist "%cd%\waifu2x-caffe\waifu2x-caffe-cui.exe" (
@@ -10,7 +10,9 @@ ECHO You have waifu2x-caffe.
 
 :dlwf
     ECHO Downloading waifu2x-caffe . . .
+    ::[TODO] Need to add a progress bar for this.
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lltcggie/waifu2x-caffe/releases/download/1.2.0.2/waifu2x-caffe.zip', 'waifu2x-caffe.zip')"
+    ::[TODO] Need to figure out error exception and  fallback to Invoke-WebRequest if WebClient.DownloadFile fails
     ::powershell Invoke-WebRequest -Uri "https://github.com/lltcggie/waifu2x-caffe/releases/download/1.2.0.2/waifu2x-caffe.zip" -OutFile "%cd%\waifu2x-caffe.zip"
     powershell Expand-Archive waifu2x-caffe.zip -DestinationPath %cd%
     ECHO You now have waifu2x-caffe
@@ -25,7 +27,9 @@ ECHO You have waifu2x-caffe.
 
 :dlim
     ECHO Downloading imagemagick . . .
+    ::[TODO] Need to add a progress bar for this.
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-7.0.8-56-portable-Q16-x64.zip', 'imagemagick.zip')"
+    ::[TODO] Need to figure out error exception and  fallback to Invoke-WebRequest if WebClient.DownloadFile fails
     ::powershell Invoke-WebRequest -Uri "ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-7.0.8-56-portable-Q16-x64.zip" -OutFile "%cd%\imagemagick.zip"
     powershell Expand-Archive imagemagick.zip -DestinationPath %cd%\imagemagick
     ECHO You now have ImageMagick.
@@ -41,53 +45,57 @@ ECHO You have waifu2x-caffe.
 :hasinput
 cls
 ECHO ! ! ! WARNING ! ! !
-ECHO We will need to purge your output folder, if you have one. Please make sure to delete/move all your files in %cd%\output
+ECHO We will need to purge your output folder, if you have one. 
+ECHO Please make sure to delete/move all your files in %cd%\output
 PAUSE
+
 ECHO Deleting and Creating %cd%\output
-del %cd%\output
-md output
+    del %cd%\output
+    md output\temp
+    cls
 
-del %cd%\input\temp
-md input\temp
-
-cls
 ECHO System check done. You have all the prerequisites :)
 PAUSE
 cls
 
-ECHO Prelimenary Questions
-ECHO ==================================================
-ECHO Do you have CUDNN installed?
-SET /p cudnn_q=Y/N?
-if %cudnn_q% == "Y" SET cudnn_mode=cudnn
-if %cudnn_q% == "y" SET cudnn_mode=cudnn
-if %cudnn_q% == "N" SET cudnn_mode=cpu
-if %cudnn_q% == "n" SET cudnn_mode=cpu
-cls
+::[TODO] This needs to be saved in a config file
+:config
+    ECHO Prelimenary Questions
+    ECHO ==================================================
+    ECHO Do you have CUDNN installed?
+    SET /p cudnn_q="Y/N? " 
+    if %cudnn_q% == "Y" SET cudnn_mode=cudnn
+    if %cudnn_q% == "y" SET cudnn_mode=cudnn
+    if %cudnn_q% == "N" SET cudnn_mode=cpu
+    if %cudnn_q% == "n" SET cudnn_mode=cpu
+    cls
 
-ECHO Do you have wish to use TTA Mode? TTA mode will increase quality but will also increase render time.
-SET /p tta_q=Y/N?
-if %tta_q% == "Y" SET tta_mode=1
-if %tta_q% == "y" SET tta_mode=1
-if %tta_q% == "N" SET tta_mode=0
-if %tta_q% == "n" SET tta_mode=0
-cls
+    ECHO Do you have wish to use TTA Mode? TTA mode will increase quality but will also increase render time.
+    SET /p tta_q="Y/N? "
+    if %tta_q% == "Y" SET tta_mode=1
+    if %tta_q% == "y" SET tta_mode=1
+    if %tta_q% == "N" SET tta_mode=0
+    if %tta_q% == "n" SET tta_mode=0
+    cls
 
 ECHO MAIN MENU
 ECHO ==================================================
-ECHO Instructions:
-ECHO In %cd% you will find an input folder. Copy or save all the images you would like to be resized and cropped to wallpapers in that folder. 
-ECHO 1 - Create TALL Wallpapers (1080x1920)
-ECHO 2 - Create WIDE Wallpapers (1920x1080)
-ECHO 3 - Create HD TALL Wallpapers (1440x2560)
-ECHO 4 - Create HD WIDE Wallpapers (2560x1440)
-ECHO 5 - Create ALL WIDE Wallpapers (1920x1080 AND 2560x1440)
-ECHO 6 - Create ALL TALL Wallpapers (1080x1920 AND 1440x2560)
-ECHO 9 - Exit
-choice /n /c:1234569 /M "Choose an option (0-96) "
+    ECHO Instructions:
+    ::[TODO] Need to flesh out the instructions.
+    ECHO In %cd% you will find an input folder. Copy or save all the images you would like to be resized and cropped to wallpapers in that folder. 
+    ECHO 1 - Create TALL Wallpapers (1080x1920)
+    ECHO 2 - Create WIDE Wallpapers (1920x1080)
+    ECHO 3 - Create HD TALL Wallpapers (1440x2560)
+    ECHO 4 - Create HD WIDE Wallpapers (2560x1440)
+    ECHO 5 - Create ALL WIDE Wallpapers (1920x1080 AND 2560x1440)
+    ECHO 6 - Create ALL TALL Wallpapers (1080x1920 AND 1440x2560)
+    ECHO 7 - Configuration
+    ECHO 8 - Open the online README
+    ECHO 9 - Exit
+    choice /n /c:1234569 /M "Choose an option (1-9) "
 
-if "%ERRORLEVEL%" == "0" (
-    goto prog_exit
+if "%ERRORLEVEL%" == "9" (
+    goto :prog_exit
 )
 
 if "%ERRORLEVEL%" == "1" (
@@ -130,16 +138,24 @@ if "%ERRORLEVEL%" == "6" (
     goto :resize
 )
 
+if "%ERRORLEVEL%" == "7" (
+    goto :config
+)
+
+if "%ERRORLEVEL%" == "8" (
+    ::[TODO] I actually need to write a README :D
+)
+
 :resize
 if %img_tall% == "1" (
-    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -h %img_height% -n 2 -m auto_scale -e png -o %cd%\input\temp -i %cd%\input
-    %cd%\imagemagick\mogrify.exe %cd%\input\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
+    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -h %img_height% -n 2 -m auto_scale -e png -o %cd%\output\temp -i %cd%\input
+    %cd%\imagemagick\mogrify.exe %cd%\output\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
 ) else (
-    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -w %img_width% -n 2 -m auto_scale -e png -o %cd%\input\temp -i %cd%\input
-    %cd%\imagemagick\mogrify.exe %cd%\input\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
+    %cd%\waifu2x-caffe\waifu2x-caffe-cui.exe -t %tta_mode% -b 128 -d 16 -p %cudnn_mode% -w %img_width% -n 2 -m auto_scale -e png -o %cd%\output\temp -i %cd%\input
+    %cd%\imagemagick\mogrify.exe %cd%\output\temp\*.png -background transparent -gravity center -extent %img_height%x%img_width% -format png *.png
 )
 
 :prog_exit
-del %cd%\input\temp
-md input\temp
+del %cd%\output\temp
+md output\temp
 PAUSE
